6e42cbf2524608ede6554f0ae9f7cf9be64aa83c
promptips <promptips@proton.me>
2023-01-30 07:26:35 +0000

    stats

--- sam2/modeling/sam/mask_decoder.py
+++ sam2/modeling/sam/mask_decoder.py
@@ -157,4 +157,5 @@ class MaskDecoder(nn.Module):
         else:
             # Take the mask output token. Here we *always* use the token for single mask output.
             # At test time, even if we track after 1-click (and using multimask_output=True),
-            # we still take the single mask 
+            # we still take the single mask token here. The rationale is that we always track
+            # after multiple clicks during training, so th
