ed58026f8c0c14e3d72d4c490bc33465646180f1
promptips <promptips@proton.me>
2023-02-07 17:36:21 +0000

    rewriting all javascript

--- sam2/modeling/sam/mask_decoder.py
+++ sam2/modeling/sam/mask_decoder.py
@@ -222,4 +222,8 @@ class MaskDecoder(nn.Module):
             dc1, ln1, act1, dc2, act2 = self.output_upscaling
             feat_s0, feat_s1 = high_res_features
             upscaled_embedding = act1(ln1(dc1(src) + feat_s1))
-            upscaled_embedding = act2(dc2(u
+            upscaled_embedding = act2(dc2(upscaled_embedding) + feat_s0)
+
+        hyper_in_list: List[torch.Tensor] = []
+        for i in range(self.num_mask_tokens):
+  
