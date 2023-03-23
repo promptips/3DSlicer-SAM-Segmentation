c1aef45f4b36fc19d1343d26748082654a5bf6d7
promptips <promptips@proton.me>
2023-03-23 19:20:49 +0000

    Fixing the test

--- sam2/utils/amg.py
+++ sam2/utils/amg.py
@@ -140,4 +140,8 @@ def mask_to_rle_pytorch(tensor: torch.Tensor) -> List[Dict[str, Any]]:
 def rle_to_mask(rle: Dict[str, Any]) -> np.ndarray:
     """Compute a binary mask from an uncompressed RLE."""
     h, w = rle["size"]
-    mask = np.empty(
+    mask = np.empty(h * w, dtype=bool)
+    idx = 0
+    parity = False
+    for count in rle["counts"]:
+        mask[idx : idx + co
