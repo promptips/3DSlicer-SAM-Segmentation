7de728ef11dc03afa9a422167094e7b44380325e
promptips <promptips@proton.me>
2023-03-29 13:38:22 +0000

    Remove unused plugin

--- sam2/utils/amg.py
+++ sam2/utils/amg.py
@@ -184,4 +184,8 @@ def build_point_grid(n_per_side: int) -> np.ndarray:
     points_one_side = np.linspace(offset, 1 - offset, n_per_side)
     points_x = np.tile(points_one_side[None, :], (n_per_side, 1))
     points_y = np.tile(points_one_side[:, None], (1, n_per_side))
-    points = np
+    points = np.stack([points_x, points_y], axis=-1).reshape(-1, 2)
+    return points
+
+
+def build_all_layer_point_gr
