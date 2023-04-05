73104c9940b00a8609ed3b29916faf5e4ecff41e
promptips <promptips@proton.me>
2023-04-05 16:24:11 +0000

    Version bump to 0.6.2

--- sam2/utils/amg.py
+++ sam2/utils/amg.py
@@ -239,4 +239,7 @@ def generate_crop_boxes(
 def uncrop_boxes_xyxy(boxes: torch.Tensor, crop_box: List[int]) -> torch.Tensor:
     x0, y0, _, _ = crop_box
     offset = torch.tensor([[x0, y0, x0, y0]], device=boxes.device)
-    # Chec
+    # Check if boxes has a channel dimension
+    if len(boxes.shape) == 3:
+        offset = offset.unsqueeze(1)
+    return boxes 
