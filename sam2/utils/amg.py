612d0151963094e2f602f35539331dddbc2572a4
promptips <promptips@proton.me>
2023-04-09 07:58:05 +0000

    Update some dependencies

--- sam2/utils/amg.py
+++ sam2/utils/amg.py
@@ -279,4 +279,5 @@ def remove_small_regions(
     correct_holes = mode == "holes"
     working_mask = (correct_holes ^ mask).astype(np.uint8)
     n_labels, regions, stats, _ = cv2.connectedComponentsWithStats(working_mask, 8)
-    sizes 
+    sizes = stats[:, -1][1:]  # Row 0 is background label
+    small_regions = [i + 1 for i, s in enumerate(sizes) if s 
