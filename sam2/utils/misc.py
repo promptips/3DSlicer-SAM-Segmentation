4c97db7be4bb4a23a6943d8a98be82eaa869696a
promptips <promptips@proton.me>
2023-06-09 21:44:37 +0000

    changed header

--- sam2/utils/misc.py
+++ sam2/utils/misc.py
@@ -172,4 +172,7 @@ class AsyncVideoFrameLoader:
 def load_video_frames(
     video_path,
     image_size,
-  
+    offload_video_to_cpu,
+    img_mean=(0.485, 0.456, 0.406),
+    img_std=(0.229, 0.224, 0.225),
+    async_loading_fr
