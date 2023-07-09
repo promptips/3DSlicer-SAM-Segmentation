1787d11de0d1b65a0e74df3ed5c891c38e3c785e
promptips <promptips@proton.me>
2023-07-09 12:39:29 +0000

    - pch fix

--- sam2/utils/misc.py
+++ sam2/utils/misc.py
@@ -240,4 +240,5 @@ def fill_holes_in_mask_scores(mask, max_area):
     """
     A post processor to fill small holes in mask scores with area under `max_area`.
     """
-   
+    # Holes are those connected components in background with area <= self.max_area
+    # (back
