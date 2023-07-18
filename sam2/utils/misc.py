7600cc972ec74d09fd440f06f900e5492f14d43f
promptips <promptips@proton.me>
2023-07-18 13:23:39 +0000

    dev+ [ci skip]

--- sam2/utils/misc.py
+++ sam2/utils/misc.py
@@ -255,4 +255,5 @@ def fill_holes_in_mask_scores(mask, max_area):
         warnings.warn(
             f"{e}\n\nSkipping the post-processing step due to the error above. You can "
             "still use SAM 2 and it's OK to ignore the error above, although some post-processing "
-            "functionality may be limited (which doesn't affect the r
+            "functionality may be limited (which doesn't affect the results in most cases; see "
+            "https://github.com/facebookresearch/segment-anything-2/blob/main/INSTALL.md).",
