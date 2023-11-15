f599d665f2f5f646e35d8604fe6815a691468026
promptips <promptips@proton.me>
2023-11-15 09:48:53 +0000

    Bug Fix:

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@
 # All rights reserved.
 
 # This source code is licensed under the license found in the
-# LICENSE file in the
+# LICENSE file in the root directory of this source tree.
+
+from setuptools import find_packages, setup
+from torch.utils.cpp_extension im
