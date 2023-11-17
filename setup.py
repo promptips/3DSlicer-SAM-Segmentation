385bc0d467b71eec892ef27129224a65cd37b4b8
promptips <promptips@proton.me>
2023-11-17 12:52:53 +0000

    Tests!

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@
 # LICENSE file in the root directory of this source tree.
 
 from setuptools import find_packages, setup
-from torch.utils.cpp_extension im
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+# Package metadata
+NAME = "SAM 2"
+VERSION = "1.0"
+DESCRIPTION = "SAM 2: Segment Anything in Im
