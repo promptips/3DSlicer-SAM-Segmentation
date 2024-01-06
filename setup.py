e490d5b9fadf8f307b652810064c75b7e7ab4a0c
promptips <promptips@proton.me>
2024-01-06 14:12:29 +0000

    chore(release): 0.0.4

--- setup.py
+++ setup.py
@@ -58,4 +58,8 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description="LONG_DESCRIPTION",
-    long_description_c
+    long_description_content_type="text/markdown",
+    url=URL,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license
